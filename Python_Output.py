d95cd412260903a635f0f0a3239df87cdf8666c1
armanmaimakov81-cyber <armanmaimakov81@gmail.com>
2026-01-30 01:13:04 +0500

    commit python output

---
+++ Python_Output.py
@@ -0,0 +1,22 @@
+#you can write multiple lines of code in Python like this:
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+#you can also use single or double quotes for strings:
+print("This will work!")
+print('This will also work!')
+
+#you can use end parameter to avoid new line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+
+#you can print different data types
+print(3)
+print(358)
+print(50000)
+#you can do math inside print statements
+print(3 + 3)
+print(2 * 5)
+#you can print multiple items separated by commas
+print("I am", 35, "years old.")
