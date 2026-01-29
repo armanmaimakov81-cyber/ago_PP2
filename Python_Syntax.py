219c897409da8e81e55dfd44b83e7d79a0ed6014
armanmaimakov81-cyber <armanmaimakov81@gmail.com>
2026-01-30 01:07:38 +0500

    commit python syntax

---
+++ Python_Syntax.py
@@ -0,0 +1,22 @@
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+# This is a simple Python script demonstrating basic syntax
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+#this is a comment 
+print("Python is fun!")
+
+
+#multiple lines
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+#with semicolons
+print("Hello"); print("How are you?"); print("Bye bye!")
+
