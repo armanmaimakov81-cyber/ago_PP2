c019ff9c6afb68047aec9d05f7348c204217ea90
armanmaimakov81-cyber <armanmaimakov81@gmail.com>
2026-02-07 04:16:41 +0500

    Add boolean examples and exercises

---
+++ Practice 2/boolean/boolean_comparison.py
@@ -0,0 +1,32 @@
+#ex1:
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+#ex2:
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+#ex3:
+x = 5
+y = 3
+
+print(x == y)
+#ex4:
+x = 5
+y = 3
+
+print(x != y)
+
+#ex5:
+x = 5
+y = 3
+
+print(x > y)
---
+++ Practice 2/boolean/boolean_operator.py
@@ -0,0 +1,20 @@
+#ex1:
+x = 5
+
+print(x > 3 and x < 10)
+#ex2:
+x = 5
+
+print(x < 5 or x > 10)
+#ex3:
+x = 5
+
+print(not(x > 3 and x < 10))
+#ex4:
+x = ["apple", "banana"]
+
+print("banana" in x)
+#ex5:
+x = ["apple", "banana"]
+
+print("pineapple" not in x)
