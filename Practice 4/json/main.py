211675f5337058919db7797ff3656cc027008d3c
armanmaimakov81-cyber <armanmaimakov81@gmail.com>
2026-02-28 08:30:16 +0500

    Adding to practice 4 json exercises

---
+++ Practice 4/json/main.py
@@ -0,0 +1,17 @@
+import json
+
+with open(r"C:\Users\acer\Documents\hefbvi\Practice 4\json\sample-data.json") as f:
+    data = json.load(f)
+
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':50} {'Description':20} {'Speed':7} {'MTU':6}")
+print("-" * 50 + " " + "-" * 20 + "  " + "-" * 6 + "  " + "-" * 6)
+
+for item in data["imdata"]:
+    attr = item["l1PhysIf"]["attributes"]
+    dn = attr["dn"]
+    descr = attr["descr"]
+    speed = attr["speed"]
+    mtu = attr["mtu"]
+    print(f"{dn:50} {descr:20} {speed:7} {mtu:6}")
