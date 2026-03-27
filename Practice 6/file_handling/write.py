55fb7dd400f5c6527723953923be094b13eece7b
armanmaimakov81-cyber <armanmaimakov81@gmail.com>
2026-03-28 02:24:05 +0500

    Adding file handling

--- Practice 6/file_handling/write.py
+++ Practice 6/file_handling/write.py
@@ -1,6 +1,5 @@
 with open("C:\\Users\\acer\\Documents\\hefbvi\\Practice 6\\file_handling\\sample.txt", "w") as f:
     f.write("Hello\n")
-    f.write("This is a sample file\n")
 
 with open("C:\\Users\\acer\\Documents\\hefbvi\\Practice 6\\file_handling\\sample.txt", "a") as f:
     f.write("New line 1\n")
